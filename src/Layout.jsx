a329fc54df16717e1085a0aec9f68a6ecc5b2499
azzmnrwebdev <azzmnrwebdev@gmail.com>
2025-06-12 01:38:54 +0700

    menambahkan auto scroll top

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,17 @@
 import "./assets/css/index.css";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import LoadingScreen from "./components/Loading";
 import NavbarComponent from "./components/Navbar";
 import FooterComponent from "./components/Footer";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <LoadingScreen />
