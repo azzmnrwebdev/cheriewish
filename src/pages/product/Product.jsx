0774ca03b2dcafc872513d40c08d2aada6e09665
azzmnrwebdev <azzmnrwebdev@gmail.com>
2025-06-11 11:05:45 +0700

    menambahkan file vercel.json

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,8 @@
+import { useState } from "react";
 import DOMPurify from "dompurify";
 import "../../assets/css/product.css";
 import { Container, Modal } from "react-bootstrap";
 import { Link, useLocation, useParams } from "react-router-dom";
-import { useState } from "react";
 
 const Product = () => {
   const { slug } = useParams();
