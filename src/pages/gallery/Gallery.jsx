5d66a112be399bd8305a2adea4f848154fd91414
azzmnrwebdev <azzmnrwebdev@gmail.com>
2025-06-28 01:23:17 +0700

    ubah foto pada gallery

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 
 const Gallery = () => {
   const [shuffledData, setShuffledData] = useState([]);
-  const [activeFilter, setActiveFilter] = useState("semua");
 
   const shuffleArray = (array) => {
     const newArray = [...array];
@@ -13,31 +12,12 @@ const Gallery = () => {
       const j = Math.floor(Math.random() * (i + 1));
       [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
     }
-
     return newArray;
   };
 
   useEffect(() => {
     setShuffledData(shuffleArray(galeries));
   }, []);
-
-  const handleFilterClick = (filterType) => {
-    setActiveFilter(filterType);
-  };
-
-  const filteredData = shuffledData.filter((item) => {
-    if (activeFilter === "semua") return true;
-    return item.type === activeFilter;
-  });
-
-  const getButtonStyle = (filterType) => {
-    const isActive = activeFilter === filterType;
-    return {
-      border: "2px solid #f5596c",
-      color: isActive ? "white" : "#f5596c",
-      backgroundColor: isActive ? "#f5596c" : "white",
-    };
-  };
 
   return (
     <section id="gallery">
@@ -47,71 +27,17 @@ const Gallery = () => {
             Gallery <span style={{ color: "#f5596c" }}>Cheriewish</span>
           </h1>
 
-          <p className="mb-0 sub-title text-body-secondary px-3 px-sm-5">
-            Koleksi Foto & Video Acara & Kegiatan Kami. Tempat dokumentasi
-            momen-momen seru, resmi, dan berkesan dari Cheriewish. Dari acara
-            besar sampai kegiatan kecil, semua kenangan terekam di sini. Lihat
-            dan nikmati setiap ceritanya!
+          <p className="mb-0 sub-title text-body px-3 px-sm-5">
+            Tempat mengabadikan momen berharga dari setiap produk yang kami
+            jual. Lihat keseruan saat berjualan dan kebahagiaan pelanggan
+            setelah membeli produk Cheriewish. Jadilah bagian dari perjalanan
+            kami dan rasakan kepuasan yang sama!
           </p>
         </div>
 
-        {/* Filter */}
-        <div className="mt-5">
-          <h6 className="text-filter text-dark d-flex align-items-center gap-2 mb-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1"
-              className="bi bi-sliders2-vertical"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M0 10.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1H3V1.5a.5.5 0 0 0-1 0V10H.5a.5.5 0 0 0-.5.5M2.5 12a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 1 0v-2a.5.5 0 0 0-.5-.5m3-6.5A.5.5 0 0 0 6 6h1.5v8.5a.5.5 0 0 0 1 0V6H10a.5.5 0 0 0 0-1H6a.5.5 0 0 0-.5.5M8 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 1 0v-2A.5.5 0 0 0 8 1m3 9.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1H14V1.5a.5.5 0 0 0-1 0V10h-1.5a.5.5 0 0 0-.5.5m2.5 1.5a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 1 0v-2a.5.5 0 0 0-.5-.5"
-              />
-            </svg>
-            Sort By
-          </h6>
-
-          <div
-            style={{ scrollbarWidth: "none" }}
-            className="d-flex justify-content-start align-items-center gap-2 overflow-x-auto"
-          >
-            <button
-              type="button"
-              style={getButtonStyle("semua")}
-              onClick={() => handleFilterClick("semua")}
-              className="btn text-nowrap px-4 rounded-pill d-flex align-items-center"
-            >
-              <small>Semua</small>
-            </button>
-            <button
-              type="button"
-              style={getButtonStyle("foto")}
-              onClick={() => handleFilterClick("foto")}
-              className="btn text-nowrap px-4 rounded-pill d-flex align-items-center"
-            >
-              <small>Foto</small>
-            </button>
-            <button
-              type="button"
-              style={getButtonStyle("video")}
-              onClick={() => handleFilterClick("video")}
-              className="btn text-nowrap px-4 rounded-pill d-flex align-items-center"
-            >
-              <small>Video</small>
-            </button>
-          </div>
-        </div>
-
         {/* Content */}
-        <div className="card-columns my-4">
-          {filteredData.map((item, index) => (
+        <div className="card-columns mt-5 mb-4">
+          {shuffledData.map((item, index) => (
             <div key={index} className="card border-0 rounded-0 bg-transparent">
               <div className="card-body p-0">
                 {item.type === "foto" ? (
@@ -120,11 +46,7 @@ const Gallery = () => {
                     alt="Foto"
                     className="img-fluid object-fit-cover rounded-3"
                   />
-                ) : (
-                  {
-                    /*  */
-                  }
-                )}
+                ) : null}
               </div>
             </div>
           ))}
--- src/utils/galeries.js
+++ src/utils/galeries.js
@@ -1,22 +1,36 @@
-import Foto1 from "../assets/images/galeries/IMG_0772.jpg";
-import Foto2 from "../assets/images/galeries/IMG_0776.jpg";
-import Foto3 from "../assets/images/galeries/IMG_0777.jpg";
-import Foto4 from "../assets/images/galeries/IMG_0778.jpg";
-import Foto5 from "../assets/images/galeries/IMG_0780.jpg";
-import Foto6 from "../assets/images/galeries/IMG_0782.jpg";
-import Foto7 from "../assets/images/galeries/IMG_0784.jpg";
-import Foto8 from "../assets/images/galeries/IMG_0788.jpg";
-import Foto9 from "../assets/images/galeries/IMG_0789.jpg";
-import Foto10 from "../assets/images/galeries/IMG_0791.jpg";
-import Foto11 from "../assets/images/galeries/IMG_0793.jpg";
-import Foto12 from "../assets/images/galeries/IMG_0794.jpg";
-import Foto13 from "../assets/images/galeries/IMG_0797.jpg";
-import Foto14 from "../assets/images/galeries/IMG_0801.jpg";
-import Foto15 from "../assets/images/galeries/IMG_0802.jpg";
-import Foto16 from "../assets/images/galeries/IMG_0803.jpg";
-import Foto17 from "../assets/images/galeries/IMG_0867.JPG";
-import Foto18 from "../assets/images/galeries/IMG_0868.JPG";
-import Foto19 from "../assets/images/galeries/IMG_0870.JPG";
+import Foto1 from "../assets/images/galeries/1F22A1D0-F8F8-4D75-A0BD-145C8F20ED45.jpg";
+import Foto2 from "../assets/images/galeries/IMG_2458.jpg";
+import Foto3 from "../assets/images/galeries/IMG_2461.jpg";
+import Foto4 from "../assets/images/galeries/IMG_2462.jpg";
+import Foto5 from "../assets/images/galeries/IMG_2463.jpg";
+import Foto6 from "../assets/images/galeries/IMG_2466.jpg";
+import Foto7 from "../assets/images/galeries/IMG_2467.jpg";
+import Foto8 from "../assets/images/galeries/IMG_2480.jpg";
+import Foto9 from "../assets/images/galeries/IMG_2483.jpg";
+import Foto10 from "../assets/images/galeries/IMG_2485.jpg";
+import Foto11 from "../assets/images/galeries/IMG_2487.jpg";
+import Foto12 from "../assets/images/galeries/IMG_2488.jpg";
+import Foto13 from "../assets/images/galeries/IMG_2489.jpg";
+import Foto14 from "../assets/images/galeries/IMG_2503.jpg";
+import Foto15 from "../assets/images/galeries/IMG_2504.jpg";
+import Foto16 from "../assets/images/galeries/IMG_2506.jpg";
+import Foto17 from "../assets/images/galeries/IMG_2507.jpg";
+import Foto18 from "../assets/images/galeries/IMG_2508.jpg";
+import Foto19 from "../assets/images/galeries/IMG_2515.jpg";
+import Foto20 from "../assets/images/galeries/IMG_2517.jpg";
+import Foto21 from "../assets/images/galeries/IMG_2521.jpg";
+import Foto22 from "../assets/images/galeries/IMG_2522.jpg";
+import Foto23 from "../assets/images/galeries/IMG_2523.jpg";
+import Foto24 from "../assets/images/galeries/IMG_2524.jpg";
+import Foto25 from "../assets/images/galeries/IMG_2531.jpg";
+import Foto26 from "../assets/images/galeries/IMG_2533.jpg";
+import Foto27 from "../assets/images/galeries/IMG_2535.jpg";
+import Foto28 from "../assets/images/galeries/IMG_2536.jpg";
+import Foto29 from "../assets/images/galeries/IMG_2537.jpg";
+import Foto30 from "../assets/images/galeries/IMG_2547.jpg";
+import Foto31 from "../assets/images/galeries/IMG_2549.jpg";
+import Foto32 from "../assets/images/galeries/IMG_2550.jpg";
+import Foto33 from "../assets/images/galeries/IMG_2551.jpg";
 
 const galeries = [
   {
@@ -94,7 +108,63 @@ const galeries = [
   {
     type: "foto",
     path: Foto19,
-  },
+  }, 
+  {
+    type: "foto",
+    path: Foto20,
+  }, 
+  {
+    type: "foto",
+    path: Foto21,
+  }, 
+  {
+    type: "foto",
+    path: Foto22,
+  }, 
+  {
+    type: "foto",
+    path: Foto23,
+  }, 
+  {
+    type: "foto",
+    path: Foto24,
+  }, 
+  {
+    type: "foto",
+    path: Foto25,
+  }, 
+  {
+    type: "foto",
+    path: Foto26,
+  }, 
+  {
+    type: "foto",
+    path: Foto27,
+  }, 
+  {
+    type: "foto",
+    path: Foto28,
+  }, 
+  {
+    type: "foto",
+    path: Foto29,
+  }, 
+  {
+    type: "foto",
+    path: Foto30,
+  }, 
+  {
+    type: "foto",
+    path: Foto31,
+  }, 
+  {
+    type: "foto",
+    path: Foto32,
+  }, 
+  {
+    type: "foto",
+    path: Foto33,
+  }, 
 ];
 
 export default galeries;
